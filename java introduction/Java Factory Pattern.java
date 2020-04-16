af7613424a3a32990e1aa1f4b3ed67863d93c3f1
Mister Parker <silentninja.p@yahoo.com>
2020-04-16 20:48:40 +0530

    57/64

---
+++ java introduction/Java Factory Pattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.security.*;
+interface Food {
+	 public String getType();
+	}
+	class Pizza implements Food {
+	 public String getType() {
+	 return "Someone ordered a Fast Food!";
+	 }
+	}
+
+	class Cake implements Food {
+
+	 public String getType() {
+	 return "Someone ordered a Dessert!";
+	 }
+	}
+	class FoodFactory {
+		public Food getFood(String order) {
+
+            if(order.equalsIgnoreCase("cake"))
+                return new Cake();
+            else
+                return new Pizza();
+
+}//End of getFood method
