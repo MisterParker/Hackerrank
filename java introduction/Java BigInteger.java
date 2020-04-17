a953cddb7ed7b76ccbae29d1fcc7e22e0d30550c
Mister Parker <silentninja.p@yahoo.com>
2020-04-17 22:27:07 +0530

    cont.

---
+++ java introduction/Java BigInteger.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        BigInteger b1 = new BigInteger(sc.nextLine());
+        BigInteger b2 = new BigInteger(sc.nextLine());
+        System.out.println(b1.add(b2));
+        System.out.println(b1.multiply(b2));
+    }
+}
